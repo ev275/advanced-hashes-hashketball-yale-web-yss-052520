8676a6b95ab6a29c48533a76458666985d508740
Enrique Valencia <enrique.valencia@yale.edu>
2020-05-22 21:46:27 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 # Write your code below game_hash
-#require 'pry'
+require 'pry'
 def game_hash
   {
     home: {
@@ -129,15 +129,10 @@ end
 
 def num_points_scored(name_input)
   game_hash.each do |side, team_info_hash|
-    #loop above works
     team_info_hash.each do |attr1, array|
-      #loop above works
       if attr1 == :players
         array.each do |player_stats|
-          #Loop above works
-          #binding.pry
           player_stats.each do |attr2, value|
-            #binding.pry
             if name_input == value
               return player_stats[:points]
             end
@@ -148,6 +143,118 @@ def num_points_scored(name_input)
   end
 end
 
+def shoe_size(name_input)
+  game_hash.each do |side, team_info_hash|
+    team_info_hash.each do |attr1, array|
+      if attr1 == :players
+        array.each do |player_stats|
+          player_stats.each do |attr2, value|
+            if name_input == value
+              return player_stats[:shoe]
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+def team_colors(team_name_input)
+  game_hash.each do |side, team_info_hash|
+    team_info_hash.each do |attr1, array|
+      if team_info_hash[attr1] == team_name_input
+        return team_info_hash[:colors]
+      end
+    end
+  end
+end
+
+def team_names
+  team_names_array= []
+  game_hash.each do |side, team_info_hash|
+    team_info_hash.each do |attr1, array|
+      if attr1 == :team_name
+        team_names_array << team_info_hash[:team_name]
+      end
+    end
+  end
+  team_names_array
+end
+
+def player_numbers(team_name_input)
+  jersey_numbers_array = []
+  game_hash.each do |side, team_info_hash|
+    team_info_hash.each do |attr1, array|
+      if team_info_hash[attr1] == team_name_input
+        team_info_hash[:players].each do |player_stats|
+          player_stats.each do |attr2, value|
+            if attr2 == :number
+              jersey_numbers_array << value
+            end
+          end
+        end
+      end
+    end
+  end
+  jersey_numbers_array
+end
+
+def player_stats(player_name_input)
+  game_hash.each do |side, team_info_hash|
+    team_info_hash.each do |attr1, array|
+      if attr1 == :players
+        array.each do |player_stats|
+          player_stats.each do |attr2, value|
+            if player_name_input == value
+              return player_stats
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  def largest_shoe_size_player
+    max_shoe_size = 0
+    max_shoe_size_player = ""
+    game_hash.each do |side, team_info_hash|
+    team_info_hash.each do |attr1, array|
+      if attr1 == :players
+        array.each do |player_stats|
+          player_stats.each do |attr2, value|
+            if (attr2 == :shoe) && (player_stats[attr2] > max_shoe_size)
+              max_shoe_size = player_stats[attr2]
+              max_shoe_size_player = player_stats[:player_name]
+            end
+          end
+        end
+      end
+    end
+    return max_shoe_size_player
+    binding.pry
+  end
+  
+  game_hash.each do |side, team_info_hash|
+    team_info_hash.each do |attr1, array|
+      if attr1 == :players
+        array.each do |player_stats|
+          player_stats.each do |attr2, value|
+            if largest_shoe_size_player == value
+              return player_stats[:rebounds]
+            end
+          end
+        end
+      end
+    end
+  end
+end
+    
+  
+  
+  
+  
           
 
 # Write code here
